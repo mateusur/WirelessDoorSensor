0fdbc084dfaf264c2681ebdcf9bb6122188c78e0
Mateusz Urbaniak <urban1998@wp.pl>
2025-04-05 10:10:41 +0200

    Read temp and himidity from in tree custom driver

--- src/main.c
+++ src/main.c
@@ -4,21 +4,24 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
-
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/gap.h>
 
 LOG_MODULE_REGISTER(Lesson2_Exercise1, LOG_LEVEL_INF);
 
-#define SLEEP_TIME_MS 500
+#define SLEEP_TIME_MS 100
 #define CUSTOM_LED DT_ALIAS(customled)
 #define CUSTOM_BUTTON DT_ALIAS(custombutton)
+#define TEMP_SENSOR DT_ALIAS(temp)
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
 
 static const struct gpio_dt_spec custom_led = GPIO_DT_SPEC_GET(CUSTOM_LED, gpios);
 static const struct gpio_dt_spec custom_button = GPIO_DT_SPEC_GET(CUSTOM_BUTTON, gpios);
+const struct device *const dev = DEVICE_DT_GET(TEMP_SENSOR);
+
 static struct gpio_callback button_cb_data;
 
 static const struct bt_data ad[] = {
@@ -84,22 +87,50 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t
 
 int main(void)
 {
-	device_is_ready(custom_led.port);
-	device_is_ready(custom_button.port);
+	// const struct device *const dev = DEVICE_DT_GET_ANY(sensirion_sht22);
+	struct sensor_value temp_value;
+	struct sensor_value humidity_value;
+	__ASSERT(dev != NULL, "Failed to get device binding");
+	__ASSERT(device_is_ready(dev), "Device %s is not ready", dev->name);
+	printk("device is %p, name is %s\n", dev, dev->name);
 
-	gpio_pin_configure_dt(&custom_led, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&custom_button, GPIO_INPUT);
+	// do_main(dev);
+
+	// device_is_ready(custom_led.port);
+	// device_is_ready(custom_button.port);
+
+	// gpio_pin_configure_dt(&custom_led, GPIO_OUTPUT_ACTIVE);
+	// gpio_pin_configure_dt(&custom_button, GPIO_INPUT);
 	
-	gpio_pin_interrupt_configure_dt(&custom_button, GPIO_INT_EDGE_TO_ACTIVE);
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(custom_button.pin)); 	
-	gpio_add_callback(custom_button.port, &button_cb_data);
+	// gpio_pin_interrupt_configure_dt(&custom_button, GPIO_INT_EDGE_TO_ACTIVE);
+	// gpio_init_callback(&button_cb_data, button_pressed, BIT(custom_button.pin)); 	
+	// gpio_add_callback(custom_button.port, &button_cb_data);
 	// gpio_add_callback_dt(&custom_button, &button_cb_data);
-	
+	int err;
+	LOG_INF("Error while fetching1");
+	LOG_ERR("Error while fetching2");
+
 	while (1)
 	{
-		k_msleep(SLEEP_TIME_MS);
+		err = sensor_sample_fetch(dev);
+		if (err) {
+			printk("sensor_sample_fetch failed ret %d\n", err);
+			return;
+		}
+		err = sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp_value);
+		if (err) {
+			printk("Temperature sensor_channel_get failed ret %d\n", err);
+			return;
+		}
+		err = sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity_value);
+		if (err) {
+		printk("Humidity sensor_channel_get failed ret %d\n", err);
+			return;
+		}
+		printk("temp: %d.%d °C \t humidity is %d.%d%% \n", temp_value.val1,
+					  temp_value.val2,humidity_value.val1, humidity_value.val2);
+		k_msleep(1000);
 	}
-	
 	// err = bt_enable(NULL);
 	// if (err)
 	// {
